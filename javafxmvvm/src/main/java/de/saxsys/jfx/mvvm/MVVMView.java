9cab5ade6fcfd5a449efcaa865a1411604741aa6
Alexander Casall <alexander.casall@gmail.com>
2013-09-15 21:48:32 +0200

    Fixed missing import

--- javafxmvvm/src/main/java/de/saxsys/jfx/mvvm/MVVMView.java
+++ javafxmvvm/src/main/java/de/saxsys/jfx/mvvm/MVVMView.java
@@ -1,47 +1,52 @@
 package de.saxsys.jfx.mvvm;
 
+import javafx.fxml.Initializable;
 
 /**
- * Abstract class for a MVVMView - you have to say which viewmodel it uses. Then you can use the embedded
- * {@link MVVMViewModel} property which is typed correctly.
+ * Abstract class for a MVVMView - you have to say which viewmodel it uses. Then
+ * you can use the embedded {@link MVVMViewModel} property which is typed
+ * correctly.
  * 
  * @author alexander.casall
  * 
- * @param <ViewModel> type
+ * @param <ViewModel>
+ *            type
  */
-public abstract class MVVMView<ViewModel extends MVVMViewModel> implements Initializable {
+public abstract class MVVMView<ViewModel extends MVVMViewModel> implements
+		Initializable {
 
-    /**
-     * Viewmodel.
-     */
-    protected ViewModel viewModel;
+	/**
+	 * Viewmodel.
+	 */
+	protected ViewModel viewModel;
 
-    /**
-     * Set the Viewmodel.
-     * 
-     * @param viewModel to set
-     */
-    public final void setViewModel(final ViewModel viewModel) {
-        beforeViewModelInitialization();
-        this.viewModel = viewModel;
-        afterViewModelInitialization();
-    }
+	/**
+	 * Set the Viewmodel.
+	 * 
+	 * @param viewModel
+	 *            to set
+	 */
+	public final void setViewModel(final ViewModel viewModel) {
+		beforeViewModelInitialization();
+		this.viewModel = viewModel;
+		afterViewModelInitialization();
+	}
 
-    /**
-     * @return the viewmodel
-     */
-    public ViewModel getViewModel() {
-        return viewModel;
-    }
+	/**
+	 * @return the viewmodel
+	 */
+	public ViewModel getViewModel() {
+		return viewModel;
+	}
 
-    /**
-     * This method is called before the viewmodel is going to be set.
-     */
-    public abstract void beforeViewModelInitialization();
+	/**
+	 * This method is called before the viewmodel is going to be set.
+	 */
+	public abstract void beforeViewModelInitialization();
 
-    /**
-     * This method is called after the viewmodel was set.
-     */
-    public abstract void afterViewModelInitialization();
+	/**
+	 * This method is called after the viewmodel was set.
+	 */
+	public abstract void afterViewModelInitialization();
 
 }
