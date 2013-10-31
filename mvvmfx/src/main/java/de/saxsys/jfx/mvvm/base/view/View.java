9d59ede53943900663590804dc197d2c7819a7ca
Alexander Casall <alexander.casall@gmail.com>
2013-10-31 13:54:17 +0100

    Consolidated Licence Headers
    https://github.com/sialcasa/mvvmFX/issues/11

--- mvvmfx/src/main/java/de/saxsys/jfx/mvvm/base/view/View.java
+++ mvvmfx/src/main/java/de/saxsys/jfx/mvvm/base/view/View.java
@@ -1,18 +1,19 @@
-/*
- * Copyright 2013 Alexander Casall - Saxonia Systems AG
- *
- *Licensed under the Apache License, Version 2.0 (the "License");
- *you may not use this file except in compliance with the License.
- *You may obtain a copy of the License at
- *
- *http://www.apache.org/licenses/LICENSE-2.0
- *
- *Unless required by applicable law or agreed to in writing, software
- *distributed under the License is distributed on an "AS IS" BASIS,
- *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *See the License for the specific language governing permissions and
- *limitations under the License.
- */
+/*******************************************************************************
+ * Copyright 2013 Alexander Casall
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package de.saxsys.jfx.mvvm.base.view;
 
 import java.lang.reflect.Array;
@@ -25,9 +26,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
 import javafx.fxml.Initializable;
+
+import javax.inject.Inject;
 
 import de.saxsys.jfx.mvvm.base.viewmodel.ViewModel;
 import de.saxsys.jfx.mvvm.di.InjectionWrapper;
@@ -61,8 +62,7 @@ public abstract class View<ViewModelType extends ViewModel> implements
 
 	@Inject
 	private InjectionWrapper injectionFacade;
-	
-	
+
 	/**
 	 * @return the View Model which represents the data that should be displayed
 	 *         by the view
