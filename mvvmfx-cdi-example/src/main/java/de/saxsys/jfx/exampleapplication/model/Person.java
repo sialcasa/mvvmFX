bf849caadd05773e9479a5393caf0adf96b436b9
Michael Thiele <michael.thiele@saxsys.de>
2013-11-01 16:48:19 +0100

    - introduced new boolean attribute "male" in Person to get right description in the ViewModel

--- mvvmfx-cdi-example/src/main/java/de/saxsys/jfx/exampleapplication/model/Person.java
+++ mvvmfx-cdi-example/src/main/java/de/saxsys/jfx/exampleapplication/model/Person.java
@@ -2,88 +2,103 @@ package de.saxsys.jfx.exampleapplication.model;
 
 import java.util.Random;
 
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
- * The class represents a Person with a firstname and a lastname. It provides
- * access with JavaFX Properties.
+ * The class represents a Person with a firstname and a lastname. It provides access with JavaFX Properties.
  * 
  * @author alexander.casall
  * 
  */
 public class Person {
 
-	private int technicalID;
-	private final StringProperty firstName = new SimpleStringProperty();
-	private final StringProperty lastName = new SimpleStringProperty();
+    private int technicalID;
+    private final StringProperty firstName = new SimpleStringProperty();
+    private final StringProperty lastName = new SimpleStringProperty();
+    private final ReadOnlyBooleanWrapper male = new ReadOnlyBooleanWrapper();
 
-	/**
-	 * Creates a person with given name.
-	 * 
-	 * @param firstName
-	 *            of person
-	 * @param lastName
-	 *            of person
-	 */
-	public Person(final String firstName, final String lastName) {
-		this.firstName.set(firstName);
-		this.lastName.set(lastName);
-	}
+    /**
+     * Creates a person with given name.
+     * 
+     * @param firstName of person
+     * @param lastName of person
+     */
+    public Person(final String firstName, final String lastName, final boolean male) {
+        this.firstName.set(firstName);
+        this.lastName.set(lastName);
+        this.male.set(male);
+    }
 
-	/**
-	 * @return firstname as {@link StringProperty}
-	 */
-	public StringProperty firstNameProperty() {
-		return firstName;
-	}
+    /**
+     * @return firstname as {@link StringProperty}
+     */
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
 
-	/**
-	 * @return lastname as {@link StringProperty}
-	 */
-	public StringProperty lastNameProperty() {
-		return lastName;
-	}
+    /**
+     * @return lastname as {@link StringProperty}
+     */
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
 
-	/**
-	 * @return firstname as {@link String}
-	 */
-	public String getFirstName() {
-		return firstNameProperty().get();
-	}
+    /**
+     * @return male as {@link ReadOnlyBooleanProperty}
+     */
+    public ReadOnlyBooleanProperty maleProperty() {
+        return male.getReadOnlyProperty();
+    }
 
-	/**
-	 * @see #getFirstName()
-	 * @param firstName
-	 */
-	public void setFirstName(final String firstName) {
-		firstNameProperty().set(firstName);
-	}
+    /**
+     * @return firstname as {@link String}
+     */
+    public String getFirstName() {
+        return firstNameProperty().get();
+    }
 
-	/**
-	 * @return lastname as {@link String}
-	 */
-	public String getLastName() {
-		return lastNameProperty().get();
-	}
+    /**
+     * @see #getFirstName()
+     * @param firstName
+     */
+    public void setFirstName(final String firstName) {
+        firstNameProperty().set(firstName);
+    }
 
-	/**
-	 * @see #getLastName()
-	 */
-	public void setLastName(final String firstName) {
-		firstNameProperty().set(firstName);
-	}
+    /**
+     * @return lastname as {@link String}
+     */
+    public String getLastName() {
+        return lastNameProperty().get();
+    }
 
-	/**
-	 * Gets the technical id.
-	 * 
-	 * @return technical id
-	 */
-	public int getId() {
-		if (technicalID == 0) {
-			technicalID = new Random().nextInt();
-		}
-		return technicalID;
-	}
+    /**
+     * @see #getLastName()
+     */
+    public void setLastName(final String firstName) {
+        firstNameProperty().set(firstName);
+    }
+
+    /**
+     * @return male as boolean
+     */
+    public boolean isMale() {
+        return male.get();
+    }
+
+    /**
+     * Gets the technical id.
+     * 
+     * @return technical id
+     */
+    public int getId() {
+        if (technicalID == 0) {
+            technicalID = new Random().nextInt();
+        }
+        return technicalID;
+    }
 
 }
--- mvvmfx-cdi-example/src/main/java/de/saxsys/jfx/exampleapplication/viewmodel/personwelcome/PersonWelcomeViewModel.java
+++ mvvmfx-cdi-example/src/main/java/de/saxsys/jfx/exampleapplication/viewmodel/personwelcome/PersonWelcomeViewModel.java
@@ -11,47 +11,44 @@ import de.saxsys.jfx.exampleapplication.model.Repository;
 import de.saxsys.jfx.mvvm.base.viewmodel.ViewModel;
 
 /**
- * ViewModel for a welcome view for a person. It provides the data which should
- * be visualized in the frontend. The tests for it can be written first. Have a
- * look on PersonWelcomeViewModelTest.
+ * ViewModel for a welcome view for a person. It provides the data which should be visualized in the frontend. The
+ * tests for it can be written first. Have a look on PersonWelcomeViewModelTest.
  * 
  * @author alexander.casall
  * 
  */
 public class PersonWelcomeViewModel implements ViewModel {
 
-	private Repository repository;
+    private final Repository repository;
 
-	// Properties which are used by the view.
-	private final StringProperty welcomeString = new SimpleStringProperty();
+    // Properties which are used by the view.
+    private final StringProperty welcomeString = new SimpleStringProperty();
 
-	/**
-	 * Create a {@link PersonWelcomeViewModel}.
-	 * 
-	 * @param repository
-	 *            repo which is used
-	 */
-	@Inject
-	public PersonWelcomeViewModel(Repository repository) {
-		this.repository = repository;
-	}
+    /**
+     * Create a {@link PersonWelcomeViewModel}.
+     * 
+     * @param repository repo which is used
+     */
+    @Inject
+    public PersonWelcomeViewModel(final Repository repository) {
+        this.repository = repository;
+    }
 
-	/**
-	 * Provides the the concated string.
-	 * 
-	 * @return
-	 */
-	public StringProperty welcomeStringProperty() {
-		return welcomeString;
-	}
+    /**
+     * Provides the the concated string.
+     * 
+     * @return
+     */
+    public StringProperty welcomeStringProperty() {
+        return welcomeString;
+    }
 
-	/**
-	 * Set Person id for the screen * @param personId for the screen
-	 */
-	public void setPersonId(int personId) {
-		final Person person = repository.getPersonById(personId);
-		welcomeString.bind(Bindings.concat("Willkommen Herr ",
-				person.lastNameProperty(), ", oder wollen Sie ",
-				person.firstNameProperty(), " genannt werden?"));
-	}
+    /**
+     * Set Person id for the screen * @param personId for the screen
+     */
+    public void setPersonId(final int personId) {
+        final Person person = repository.getPersonById(personId);
+        welcomeString.bind(Bindings.concat("Willkommen " + (person.isMale() ? "Herr " : "Frau "),
+                person.lastNameProperty(), ", oder wollen Sie ", person.firstNameProperty(), " genannt werden?"));
+    }
 }
