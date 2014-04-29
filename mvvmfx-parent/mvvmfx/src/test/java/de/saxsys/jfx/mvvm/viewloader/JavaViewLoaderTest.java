d3d4e88256b2a9b49ab8e05d06d9e07afff60d00
Manuel Mauky <manuel.mauky@googlemail.com>
2014-04-29 17:26:16 +0200

    #61 Implemented implicit injection of ResourceBundle and invokation of initialize method

--- mvvmfx-parent/mvvmfx/src/test/java/de/saxsys/jfx/mvvm/viewloader/JavaViewLoaderTest.java
+++ mvvmfx-parent/mvvmfx/src/test/java/de/saxsys/jfx/mvvm/viewloader/JavaViewLoaderTest.java
@@ -15,14 +15,18 @@
  ******************************************************************************/
 package de.saxsys.jfx.mvvm.viewloader;
 
+import de.saxsys.jfx.mvvm.api.JavaView;
 import de.saxsys.jfx.mvvm.viewloader.example.TestJavaView;
+import de.saxsys.jfx.mvvm.viewloader.example.TestJavaViewWithImplicitInit;
 import de.saxsys.jfx.mvvm.viewloader.example.TestViewModel;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.net.URL;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
@@ -59,5 +63,138 @@ public class JavaViewLoaderTest {
 
         assertThat(codeBehind.viewModelWasNull).isFalse();
     }
+
+    /**
+     * The purpose of this test case is to verify that the implicit initialization 
+     * for java views is working the in a similar way as it does for fxml views by the {@link javafx.fxml.FXMLLoader}.
+     * 
+     * The naming convention is:
+     * <ul>
+     *     
+     * <li>a public field of type {@link java.util.ResourceBundle} named "resources" gets the current ResourceBundle injected.</li>
+     * <li>a public no-arg method named "initialize" is called after the injection of other resources is finished.</li>
+     * 
+     * </ul>
+     * 
+     * The third convention of the FXMLLoader to inject the path of the FXML file to a public field of type {@link java.net.URL} named "location"
+     * is NOT done by mvvmfx because it doesn't make sense for Java written Views (as there is no FXML file at all).
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testImplicitInitialization() throws IOException{
+        ResourceBundle resourceBundle = new PropertyResourceBundle(new StringReader(""));
+
+        ViewTuple<TestViewModel> viewTuple = javaViewLoader.loadJavaViewTuple(TestJavaViewWithImplicitInit.class, resourceBundle);
+
+        assertThat(viewTuple).isNotNull();
+
+        assertThat(viewTuple.getView()).isNotNull().isInstanceOf(VBox.class);
+        assertThat(viewTuple.getCodeBehind()).isNotNull();
+
+        TestJavaViewWithImplicitInit codeBehind = (TestJavaViewWithImplicitInit) viewTuple.getCodeBehind();
+        assertThat(codeBehind.viewModel).isNotNull();
+        assertThat(codeBehind.resources).isEqualTo(resourceBundle);
+
+        assertThat(codeBehind.wasInitialized).isTrue();
+        assertThat(codeBehind.viewModelWasNull).isFalse();
+        assertThat(codeBehind.resourcesWasNull).isFalse();
+        
+    }
+
     
+    
+    /**
+     * Like the conventions of the FXMLLoader the implicit initialization and injection of ResourceBundles should not be made when
+     * the {@link javafx.fxml.Initializable} interface is NOT implemented.
+     */
+    @Test
+    public void implicitInitializationShouldNotBeExecutedWhenInitializableInterfaceIsImplemented(){
+        
+        ResourceBundle noResourceBundle = null;
+
+        TestView view = (TestView)javaViewLoader.loadJavaViewTuple(TestView.class, 
+                noResourceBundle).getCodeBehind();
+
+        assertThat(view.explicitInitWasCalled).isTrue();
+        assertThat(view.implicitInitWasCalled).isFalse();
+    }
+
+    /**
+     * This test class is used by {@link #implicitInitializationShouldNotBeExecutedWhenInitializableInterfaceIsImplemented()}.
+     */
+    public static class TestView extends VBox implements JavaView<TestViewModel>, Initializable{
+
+        public boolean explicitInitWasCalled = false;
+        public boolean implicitInitWasCalled = false;
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            explicitInitWasCalled = true;
+        }
+
+        public void initialize(){
+            implicitInitWasCalled = true;
+        }
+    }
+
+    /**
+     * The implicit initialize method should only be called when it is defined to be public.
+     */
+    @Test
+    public void implicitInitializeShouldOnlyBeExecutedWhenItsPublic(){
+        
+        class TestViewProtectedInitialize extends VBox implements JavaView<TestViewModel>{
+            public boolean implicitInitWasCalled = false;
+            
+            protected void initialize(){
+                implicitInitWasCalled = true;
+            }
+        }
+        
+        TestViewProtectedInitialize view = new TestViewProtectedInitialize();
+        javaViewLoader.callInitialize(view);
+        
+        assertThat(view.implicitInitWasCalled).isFalse();
+    }
+
+    /**
+     * The ResourceBundle should only be injected implicitly when the field is declared public.
+     */
+    @Test
+    public void implicitInjectionOfResourceBundleShouldOnlyBeDoneWhenItIsPublic() throws IOException{
+
+        class TestViewProtectedInitialize extends VBox implements JavaView<TestViewModel>{
+            protected ResourceBundle resources;
+        }
+        
+        TestViewProtectedInitialize view = new TestViewProtectedInitialize();
+
+        ResourceBundle resourceBundle = new PropertyResourceBundle(new StringReader(""));
+        javaViewLoader.injectResourceBundle(view,resourceBundle);
+
+        assertThat(view.resources).isNull();
+    }
+
+
+    /**
+     * According to the conventions of the {@link javafx.fxml.FXMLLoader} the method "initialize"
+     * is only invoked implicitly when it has no parameters.
+     */
+    @Test
+    public void implicitInitializeMethodMayNotHaveParameters(){
+        class TestViewInitializeWithParams extends VBox implements JavaView<TestViewModel>{
+            public boolean implicitInitWasCalled = false;
+
+            public void initialize(URL location, ResourceBundle resources){
+                implicitInitWasCalled = true;
+            }
+        }
+
+        TestViewInitializeWithParams view = new TestViewInitializeWithParams();
+        javaViewLoader.callInitialize(view);
+
+        assertThat(view.implicitInitWasCalled).isFalse();
+    }
+
 }
