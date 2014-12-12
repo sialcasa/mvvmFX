388297bf3b002cc1c9ac938dc5130dd9419621e7
Manuel Mauky <manuel.mauky@googlemail.com>
2014-12-12 14:40:43 +0100

    Add script for maven central release deployment

---
+++ deploy_release.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Script for the deployment of releases to maven central
+#
+# Expecting a "settings-mvvmfx.xml" file in maven_home with the authentication configuration for the central repository.
+#
+# The settings file should look something like this:
+#
+# <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+#   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+#   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+#   http://maven.apache.org/xsd/settings-1.0.0.xsd">
+#
+#   <servers>
+#     <server>
+#       <id>sonatype-nexus-snapshots</id>
+#       <username>YOUR USERNAME</username>
+#       <password>>OUR PASSWORD</password>
+#     </server>
+#     <server>
+#       <id>sonatype-nexus-staging</id>
+#       <username>YOUR USERNAME</username>
+#       <password>YOUR PASSWORD</password>
+#     </server>
+#   </servers>
+# </settings>
+
+mvn clean deploy -pl 'mvvmfx,mvvmfx-cdi,mvvmfx-guice,mvvmfx-archetype' -am -DskipTests=true -Pdeploy-release --settings ~/.m2/settings-mvvmfx.xml
