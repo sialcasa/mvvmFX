eec917862b226339040c4af62388fce47ac84792
Manuel Mauky <manuel.mauky@saxsys.de>
2015-03-13 16:13:23 +0100

    #189 Add ViewListFactory for todo items

--- examples/mvvmfx-todomvc/src/main/java/de/saxsys/mvvmfx/example/todomvc/ui/item/ItemOverviewView.java
+++ examples/mvvmfx-todomvc/src/main/java/de/saxsys/mvvmfx/example/todomvc/ui/item/ItemOverviewView.java
@@ -1,23 +1,29 @@
 package de.saxsys.mvvmfx.example.todomvc.ui.item;
 
-import de.saxsys.mvvmfx.FxmlView;
-import de.saxsys.mvvmfx.InjectViewModel;
+import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.utils.viewlist.ViewListCellFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+
+import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
 
 /**
  * @author manuel.mauky
  */
 public class ItemOverviewView implements FxmlView<ItemOverviewViewModel> {
-
+	
 	@FXML
 	public ListView<ItemViewModel> items;
-
+	
 	@InjectViewModel
 	private ItemOverviewViewModel viewModel;
 	
-	public void initialize(){
+	public void initialize() {
 		items.setItems(viewModel.itemsProperty());
+		
+		final ViewListCellFactory<ItemViewModel> cellFactory = viewModel -> FluentViewLoader.fxmlView(ItemView.class).viewModel(viewModel).load();
+		items.setCellFactory(cellFactory);
 	}
 	
 }
