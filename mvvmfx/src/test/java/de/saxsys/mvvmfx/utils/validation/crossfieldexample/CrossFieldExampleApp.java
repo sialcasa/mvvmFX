ef379e9a3ecf7cd04d578737792db52003a5d0a0
Manuel Mauky <manuel.mauky@saxsys.de>
2015-06-10 15:18:42 +0200

    #194 add example for cross field validation

---
+++ mvvmfx/src/test/java/de/saxsys/mvvmfx/utils/validation/crossfieldexample/CrossFieldExampleApp.java
@@ -0,0 +1,30 @@
+package de.saxsys.mvvmfx.utils.validation.crossfieldexample;
+
+import de.saxsys.mvvmfx.FluentViewLoader;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ * @author manuel.mauky
+ */
+public class CrossFieldExampleApp extends Application {
+
+	public static void main(String... args) {
+		launch(args);
+	}
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		final Parent root = FluentViewLoader.fxmlView(RegisterFormView.class).load().getView();
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+		
+	}
+}
