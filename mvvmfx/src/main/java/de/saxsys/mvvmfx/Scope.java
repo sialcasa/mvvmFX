a9213185c22a26a3359f317e39b379081229e003
Manuel Mauky <manuel.mauky@googlemail.com>
2015-11-03 23:10:46 +0100

    add notification mechanism to scopes

--- mvvmfx/src/main/java/de/saxsys/mvvmfx/Scope.java
+++ mvvmfx/src/main/java/de/saxsys/mvvmfx/Scope.java
@@ -1,5 +1,7 @@
 package de.saxsys.mvvmfx;
 
+import de.saxsys.mvvmfx.utils.notifications.NotificationObserver;
+
 /**
  * Scope.
  * 
@@ -7,5 +9,21 @@ package de.saxsys.mvvmfx;
  *
  */
 public interface Scope {
-	
+
+    default void publish(String messageName, Object... payload) {
+        MvvmFX.getNotificationCenter().publish(this, messageName, payload);
+    }
+
+    default void subscribe(String messageName, NotificationObserver observer) {
+        MvvmFX.getNotificationCenter().subscribe(this, messageName, observer);
+    }
+
+    default void unsubscribe(String messageName, NotificationObserver observer) {
+        MvvmFX.getNotificationCenter().unsubscribe(this, messageName, observer);
+    }
+
+    default void unsubscribe(NotificationObserver observer) {
+        MvvmFX.getNotificationCenter().unsubscribe(this, observer);
+    }
+
 }
