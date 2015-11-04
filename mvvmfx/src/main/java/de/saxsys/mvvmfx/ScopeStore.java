95d9e1bc6f338944857fd4155b956d7e6fa11a17
Alexander Casall <alexander.casall@gmail.com>
2015-11-04 10:10:09 +0100

    WeakHashMap in Scope store

--- mvvmfx/src/main/java/de/saxsys/mvvmfx/ScopeStore.java
+++ mvvmfx/src/main/java/de/saxsys/mvvmfx/ScopeStore.java
@@ -1,45 +1,44 @@
 package de.saxsys.mvvmfx;
 
-import de.saxsys.mvvmfx.internal.viewloader.DependencyInjector;
-
-import java.util.HashMap;
 import java.util.Map;
+
+import de.saxsys.mvvmfx.internal.WeakValueHashMap;
+import de.saxsys.mvvmfx.internal.viewloader.DependencyInjector;
 
 /**
  * Scope Store.
  * 
  * @author alexander.casall
- *		
+ * 		
  */
 public class ScopeStore {
 	
-	// TODO Memory Leak fixen?
-	private final Map<String, Scope> scopes = new HashMap<>();
+	private final Map<String, Scope> scopes = new WeakValueHashMap();
 	
 	private static final ScopeStore INSTANCE = new ScopeStore();
 	
 	public static ScopeStore getInstance() {
 		return INSTANCE;
 	}
-
-    public <V extends Scope> V getScope(Class<V> scopeType) {
-        return getScope(scopeType, "");
-    }
-
-    public <V extends Scope> V getScope(Class<V> scopeType, String id) {
-        String mapId = scopeType.getName() + id.trim();
-
-
-        if(! getInstance().scopes.containsKey(mapId)) {
-            V scope = getInstance().createScopeInstance(scopeType);
-            getInstance().scopes.put(mapId, scope);
-        }
-
-        final V v = (V) getInstance().scopes.get(mapId);
-
-        return v;
-    }
-
+	
+	public <V extends Scope> V getScope(Class<V> scopeType) {
+		return getScope(scopeType, "");
+	}
+	
+	public <V extends Scope> V getScope(Class<V> scopeType, String id) {
+		String mapId = scopeType.getName() + id.trim();
+		
+		
+		if (!getInstance().scopes.containsKey(mapId)) {
+			V scope = getInstance().createScopeInstance(scopeType);
+			getInstance().scopes.put(mapId, scope);
+		}
+		
+		final V v = (V) getInstance().scopes.get(mapId);
+		
+		return v;
+	}
+	
 	private <V extends Scope> V createScopeInstance(Class<V> scopeType) {
 		return DependencyInjector.getInstance().getInstanceOf(scopeType);
 	}
