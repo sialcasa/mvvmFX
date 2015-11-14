c8459d82c322176e917f8ff50febbde94d288024
Alexander Casall <alexander.casall@gmail.com>
2015-11-14 17:17:04 +0100

    code formatting

--- examples/contacts-example/src/main/java/de/saxsys/mvvmfx/examples/contacts/ui/addressform/AddressFormView.java
+++ examples/contacts-example/src/main/java/de/saxsys/mvvmfx/examples/contacts/ui/addressform/AddressFormView.java
@@ -1,27 +1,29 @@
 package de.saxsys.mvvmfx.examples.contacts.ui.addressform;
 
+import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
-import de.saxsys.mvvmfx.FxmlView;
-import de.saxsys.mvvmfx.InjectViewModel;
 
 public class AddressFormView implements FxmlView<AddressFormViewModel> {
+	
 	@FXML
 	public TextField streetInput;
 	@FXML
 	public TextField postalcodeInput;
 	@FXML
 	public TextField cityInput;
+	
 	@FXML
 	public ComboBox<String> countryInput;
 	@FXML
 	public ComboBox<String> federalStateInput;
+	
 	@FXML
 	public Label subdivisionLabel;
-	
 	@FXML
 	public Label countryLabel;
 	
@@ -34,9 +36,7 @@ public class AddressFormView implements FxmlView<AddressFormViewModel> {
 	
 	
 	public void initialize() {
-		subdivisionLabel.textProperty().bind(viewModel.subdivisionLabel());
-		subdivisionLabel.disableProperty().bind(viewModel.subdivisionInputDisabledProperty());
-		
+		loadingIndicator.visibleProperty().bind(viewModel.loadingInProgressProperty());
 		countryLabel.disableProperty().bind(viewModel.countryInputDisabledProperty());
 		
 		streetInput.textProperty().bindBidirectional(viewModel.streetProperty());
@@ -51,7 +51,7 @@ public class AddressFormView implements FxmlView<AddressFormViewModel> {
 		federalStateInput.valueProperty().bindBidirectional(viewModel.selectedSubdivisionProperty());
 		federalStateInput.disableProperty().bind(viewModel.subdivisionInputDisabledProperty());
 		
-		loadingIndicator.visibleProperty().bind(viewModel.loadingInProgressProperty());
-		
+		subdivisionLabel.textProperty().bind(viewModel.subdivisionLabel());
+		subdivisionLabel.disableProperty().bind(viewModel.subdivisionInputDisabledProperty());
 	}
 }
