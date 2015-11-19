87af12eb31d9316d73c563bec3b9e55237dab0c1
Manuel Mauky <manuel.mauky@googlemail.com>
2015-11-19 15:15:55 +0100

    failing test to reproduce guice aop bug

---
+++ mvvmfx-guice/src/test/java/de/saxsys/mvvmfx/guice/interceptiontest/InterceptedView.java
@@ -0,0 +1,23 @@
+package de.saxsys.mvvmfx.guice.interceptiontest;
+
+import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
+
+public class InterceptedView implements FxmlView<InterceptedViewModel> {
+    public static boolean vmWasInjected = false;
+    public static boolean wasInitCalled = false;
+
+    @InjectViewModel
+    private InterceptedViewModel viewModel;
+
+    public InterceptedView() {
+        System.out.println("new InterceptedView()");
+    }
+
+    public void initialize() {
+        System.out.println("init");
+        wasInitCalled = true;
+        vmWasInjected = viewModel != null;
+    }
+
+}
