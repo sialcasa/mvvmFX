e756b3796db2f4e4f46b3bb6e68f842b3706efbe
PRo <peter.rogge@yahoo.de>
2016-01-31 19:23:52 +0100

    Format sourcecode.

--- examples/mini-examples/helloworld/src/main/java/de/saxsys/mvvmfx/examples/helloworld/HelloWorldViewModel.java
+++ examples/mini-examples/helloworld/src/main/java/de/saxsys/mvvmfx/examples/helloworld/HelloWorldViewModel.java
@@ -6,19 +6,19 @@ import javafx.beans.property.StringProperty;
 import de.saxsys.mvvmfx.ViewModel;
 
 public class HelloWorldViewModel implements ViewModel {
-	
-	private StringProperty helloMessage = new SimpleStringProperty("Hello World");
-	
-	public StringProperty helloMessage() {
-		return helloMessage;
-	}
-	
-	public String getHelloMessage() {
-		return helloMessage.get();
-	}
-	
-	public void setHelloMessage(String message) {
-		helloMessage.set(message);
-	}
-	
+
+    private final StringProperty helloMessage = new SimpleStringProperty("Hello World");
+
+    public StringProperty helloMessage() {
+        return helloMessage;
+    }
+
+    public String getHelloMessage() {
+        return helloMessage.get();
+    }
+
+    public void setHelloMessage(String message) {
+        helloMessage.set(message);
+    }
+
 }
