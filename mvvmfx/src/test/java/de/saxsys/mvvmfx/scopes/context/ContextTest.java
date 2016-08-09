085590b866d774d0802f1d582d0c71678d303f7f
Manuel Mauky <manuel.mauky@saxsys.de>
2016-08-09 16:29:45 +0200

    add test case to reproduce bug #426
    When an existing ViewModel instance is provided to the FluentViewLoader, the context isn't injected into the Root view.

---
+++ mvvmfx/src/test/java/de/saxsys/mvvmfx/scopes/context/ContextTest.java
@@ -0,0 +1,33 @@
+package de.saxsys.mvvmfx.scopes.context;
+
+import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.scopes.context.views.ScopedFxmlView;
+import de.saxsys.mvvmfx.scopes.context.views.ScopedFxmlViewModel;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContextTest {
+
+
+	/**
+	 * Due to a bug (<a href="https://github.com/sialcasa/mvvmFX/issues/426">#426</a>)
+	 * the context wasn't injected when an existing ViewModel instance was provided to
+	 * the {@link FluentViewLoader}.
+	 */
+	@Test
+	public void testInjectContextWithProvidedViewModel() {
+
+		ScopedFxmlViewModel viewModel = new ScopedFxmlViewModel();
+
+		ScopedFxmlView codeBehind = FluentViewLoader
+				.fxmlView(ScopedFxmlView.class)
+				.viewModel(viewModel)
+				.load()
+				.getCodeBehind();
+
+		assertThat(codeBehind.context).isNotNull();
+
+	}
+
+}
