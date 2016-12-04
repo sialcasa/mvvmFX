00ba18e1695c2cc215be95614497db5718cfb6ef
Manuel Mauky <manuel.mauky@googlemail.com>
2016-12-04 02:15:06 +0100

    add possibility for more complex rules to observables validator #443

--- mvvmfx/src/main/java/de/saxsys/mvvmfx/utils/validation/ObservableRuleBasedValidator.java
+++ mvvmfx/src/main/java/de/saxsys/mvvmfx/utils/validation/ObservableRuleBasedValidator.java
@@ -22,28 +22,46 @@ import java.util.List;
 
 
 /**
- * This {@link Validator} implementation uses observable boolean values as rules. In comparison to the
- * {@link FunctionBasedValidator} with this implementation more complex validation logic can be implemented.
+ * This {@link Validator} implementation uses observable values as rules. Compared to the {@link FunctionBasedValidator}
+ * this allows more complex validation logic.
  * <p>
- * It is useful for use cases where:
- *
+ * There are two variants of rules possible:
  * <ul>
- * <li>the validation logic is already available as observable boolean</li>
- * <li>you need to define more multiple rules</li>
- * <li>you need to define complex rules that f.e. are considering multiple fields (cross-field-validation)</li>
+ *  <li>"boolean rules" of type ObservableValue&lt;Boolean&gt;</li>
+ *  <li>"complex rules" of type ObservableValue&lt;ValidationMessage&gt;</li>
  * </ul>
+ *
+ * <h2>Boolean Rules of type ObservableValue&lt;Boolean&gt;</h2>
+ *
+ * The first variant uses an {@link ObservableValue<Boolean>} together with a static message. If this observable
+ * has a value of <code>false</code> the validation status will be "invalid" and the given message will be present in the {@link ValidationStatus}
+ * of this validator.
+ * If the observable has a value of <code>true</code> it is considered to be valid.
+ *
+ *
+ * <h2>Complex Rules of type ObservableValue&lt;ValidationMessage&gt;</h2>
+ *
+ * The second variant allows more complex rules. It uses a {@link ObservableValue<ValidationMessage>} as rule.
+ * If this observable has a value other then <code>null</code> it is considered to be invalid. The {@link ValidationMessage}
+ * value will be present in the {@link ValidationStatus} of this validator.
+ *
  * <p>
- * You can add multiple rules via the {@link #addRule(ObservableValue, ValidationMessage)} method. Each rule is an
- * observable boolean value. If the rule evaluates to <code>true</code> it is considered to be "valid" and no message
- * will be present in the {@link ValidationStatus} ({@link #getValidationStatus()}). If the rule evaluates to
- * <code>false</code> it is considered to be "invalid". In this case the given {@link ValidationMessage} will be
- * present.
- * <p>
+ *
+ * You can add multiple rules via the {@link #addRule(ObservableValue, ValidationMessage)} and {@link #addRule(ObservableValue)} method.
  * If multiple rules are violated, each message will be present.
  */
 public class ObservableRuleBasedValidator implements Validator {
-	
-	private List<ObservableValue<Boolean>> rules = new ArrayList<>();
+
+    /*
+    These lists are used to store the rules that this validator is working with.
+    The reason for this is to prevent problems with garbage collection.
+    If the validator wouldn't keep references to all given rules it would be possible that they are
+    removed by the garbage collector which would result in wrong validation results.
+    To prevent this we store references to all given rules here.
+    Don't get confused by the fact that no values are only added to these lists but not obtained.
+     */
+	private List<ObservableValue<Boolean>> booleanRules = new ArrayList<>();
+    private List<ObservableValue<ValidationMessage>> complexRules = new ArrayList<>();
 	
 	private ValidationStatus validationStatus = new ValidationStatus();
 	
@@ -62,16 +80,16 @@ public class ObservableRuleBasedValidator implements Validator {
 	 * @param message
 	 */
 	public void addRule(ObservableValue<Boolean> rule, ValidationMessage message) {
-		rules.add(rule);
-		
+	    booleanRules.add(rule);
+
 		rule.addListener((observable, oldValue, newValue) -> {
-			validateRule(newValue, message);
+			validateBooleanRule(newValue, message);
 		});
-		
-		validateRule(rule.getValue(), message);
+
+		validateBooleanRule(rule.getValue(), message);
 	}
 	
-	private void validateRule(boolean isValid, ValidationMessage message) {
+	private void validateBooleanRule(boolean isValid, ValidationMessage message) {
 		if (isValid) {
 			validationStatus.removeMessage(message);
 		} else {
@@ -83,5 +101,35 @@ public class ObservableRuleBasedValidator implements Validator {
 	public ValidationStatus getValidationStatus() {
 		return validationStatus;
 	}
-	
+
+    /**
+     * Add a complex rule for this validator.
+     * <p>
+     * The rule is defined by an {@link ObservableValue}. If this observable contains a {@link ValidationMessage}
+     * object the rule is considered to be violated and the {@link ValidationStatus} of this validator will contain
+     * the validation message object contained in the observable value.
+     * If the observable doesn't contain a value (in other words it contains <code>null</code>) the rule is considered to
+     * be fulfilled and the validation status of this validator will be valid (given that no other rule is violated).
+     * <p>
+     *
+     *  This method allows some more complex rules compared to {@link #addRule(ObservableValue, ValidationMessage)}.
+     *  This way you can define rules that have different messages for specific cases.
+     *
+     * @param rule
+     */
+    public void addRule(ObservableValue<ValidationMessage> rule) {
+        complexRules.add(rule);
+
+        rule.addListener((observable, oldValue, newValue) -> {
+            if(newValue == null) {
+               validationStatus.removeMessage(oldValue);
+            } else {
+                validationStatus.addMessage(newValue);
+            }
+        });
+
+        if(rule.getValue() != null) {
+            validationStatus.addMessage(rule.getValue());
+        }
+    }
 }
--- mvvmfx/src/test/java/de/saxsys/mvvmfx/utils/validation/ObservableRulesValidatorTest.java
+++ mvvmfx/src/test/java/de/saxsys/mvvmfx/utils/validation/ObservableRulesValidatorTest.java
@@ -15,10 +15,12 @@
  ******************************************************************************/
 package de.saxsys.mvvmfx.utils.validation;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
+import de.saxsys.mvvmfx.testingutils.GCVerifier;
+import javafx.beans.property.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,26 +28,64 @@ public class ObservableRulesValidatorTest {
 	
 	private BooleanProperty rule1;
 	private BooleanProperty rule2;
-	
+    private ObjectProperty<ValidationMessage> rule3;
+    private ObjectProperty<ValidationMessage> rule4;
+
 	private ObservableRuleBasedValidator validator;
-	
+
 	@Before
 	public void setup() {
 		rule1 = new SimpleBooleanProperty();
 		rule2 = new SimpleBooleanProperty();
-		
+
+        rule3 = new SimpleObjectProperty<>();
+        rule4 = new SimpleObjectProperty<>();
+
 		validator = new ObservableRuleBasedValidator();
 	}
-	
+
+    /**
+     * This test is used to reproduce a special behavior when garbage collection is done after
+     * adding rules.
+     * The validator implementation has to keep references to the given rules (for example by storing
+     * them in a list). Otherwise they may be removed by the garbage collector. In this case they wouldn't be
+     * evaluated again after a value has changed.
+     */
 	@Test
-	public void test() {
-		
+    public void testGarbageCollection() {
+        StringProperty value = new SimpleStringProperty();
+        final ValidationStatus status = validator.getValidationStatus();
+        Pattern uppercasePattern = Pattern
+                .compile("[A-Z]+");
+
+        validator.addRule(ObservableRules.notEmpty(value), ValidationMessage.error("not empty"));
+        validator.addRule(ObservableRules.matches(value, uppercasePattern), ValidationMessage.error("not matching"));
+
+        GCVerifier.forceGC();
+
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).hasSize(2);
+
+        value.setValue("something");
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).hasSize(1);
+
+        value.setValue("SOMETHING");
+        assertThat(status.isValid()).isTrue();
+        assertThat(status.getErrorMessages()).isEmpty();
+    }
+
+
+	@Test
+	public void testBooleanRules() {
 		final ValidationStatus status = validator.getValidationStatus();
 		assertThat(status.isValid()).isTrue();
 		
 		rule1.setValue(false);
 		
 		validator.addRule(rule1, ValidationMessage.error("error"));
+
+        GCVerifier.forceGC();
 		
 		assertThat(status.isValid()).isFalse();
 		assertThat(status.getErrorMessages()).hasSize(1);
@@ -58,6 +98,8 @@ public class ObservableRulesValidatorTest {
 		rule2.setValue(false);
 		
 		validator.addRule(rule2, ValidationMessage.warning("warning"));
+
+        GCVerifier.forceGC();
 		
 		assertThat(status.isValid()).isFalse();
 		assertThat(status.getErrorMessages()).isEmpty();
@@ -79,7 +121,115 @@ public class ObservableRulesValidatorTest {
 		assertThat(status.getErrorMessages()).isEmpty();
 		assertThat(status.getWarningMessages()).isEmpty();
 	}
-	
+
+
+	@Test
+    public void testComplexRules() {
+        final ValidationStatus status = validator.getValidationStatus();
+
+        validator.addRule(rule3);
+        validator.addRule(rule4);
+
+        GCVerifier.forceGC();
+
+        assertThat(status.isValid()).isTrue();
+        assertThat(status.getErrorMessages()).isEmpty();
+        assertThat(status.getWarningMessages()).isEmpty();
+
+
+        // when
+        rule3.setValue(ValidationMessage.error("Error"));
+
+        // then
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).hasSize(1);
+        assertThat(status.getErrorMessages().get(0).getMessage()).isEqualTo("Error");
+        assertThat(status.getWarningMessages()).isEmpty();
+
+        // when
+        rule4.setValue(ValidationMessage.warning("Warning"));
+
+        // then
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).hasSize(1);
+        assertThat(status.getErrorMessages().get(0).getMessage()).isEqualTo("Error");
+        assertThat(status.getWarningMessages()).hasSize(1);
+        assertThat(status.getWarningMessages().get(0).getMessage()).isEqualTo("Warning");
+
+        // when
+        rule3.setValue(null);
+
+        // then
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).isEmpty();
+        assertThat(status.getWarningMessages()).hasSize(1);
+        assertThat(status.getWarningMessages().get(0).getMessage()).isEqualTo("Warning");
+
+        // when
+        rule4.setValue(null);
+
+        // then
+        assertThat(status.isValid()).isTrue();
+        assertThat(status.getErrorMessages()).isEmpty();
+        assertThat(status.getWarningMessages()).isEmpty();
+    }
+
+    @Test
+    public void testBooleanAndComplexRulesCombined() {
+        final ValidationStatus status = validator.getValidationStatus();
+
+
+        rule1.setValue(true);
+        rule2.setValue(true);
+
+        validator.addRule(rule1, ValidationMessage.error("error"));
+        validator.addRule(rule2, ValidationMessage.warning("warning"));
+
+        validator.addRule(rule3);
+        validator.addRule(rule4);
+
+        GCVerifier.forceGC();
+
+        assertThat(status.isValid()).isTrue();
+        assertThat(status.getErrorMessages()).isEmpty();
+        assertThat(status.getWarningMessages()).isEmpty();
+
+        // when
+        rule1.setValue(false);
+        rule3.setValue(ValidationMessage.error("other error"));
+
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).hasSize(2);
+        assertThat(status.getErrorMessages().get(0).getMessage()).isEqualTo("error");
+        assertThat(status.getErrorMessages().get(1).getMessage()).isEqualTo("other error");
+        assertThat(status.getWarningMessages()).isEmpty();
+
+
+        // when
+        rule2.setValue(false);
+        rule4.setValue(ValidationMessage.warning("other warning"));
+
+        assertThat(status.isValid()).isFalse();
+        assertThat(status.getErrorMessages()).hasSize(2);
+        assertThat(status.getErrorMessages().get(0).getMessage()).isEqualTo("error");
+        assertThat(status.getErrorMessages().get(1).getMessage()).isEqualTo("other error");
+        assertThat(status.getWarningMessages()).hasSize(2);
+        assertThat(status.getWarningMessages().get(0).getMessage()).isEqualTo("warning");
+        assertThat(status.getWarningMessages().get(1).getMessage()).isEqualTo("other warning");
+
+
+        // when
+        rule1.setValue(true);
+        rule2.setValue(true);
+        rule3.setValue(null);
+        rule4.setValue(null);
+
+        // then
+        assertThat(status.isValid()).isTrue();
+        assertThat(status.getErrorMessages()).isEmpty();
+        assertThat(status.getWarningMessages()).isEmpty();
+
+    }
 	
 	
 }
