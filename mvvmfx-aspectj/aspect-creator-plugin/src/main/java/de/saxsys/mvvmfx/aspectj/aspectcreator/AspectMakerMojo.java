4ac8703d0cc3db07fb3cbae3c7e969a8122fbe9c
gbalderas <gerardo.balderas@ws072.saxsys.de>
2016-12-13 18:12:34 +0100

    remove null checks

--- mvvmfx-aspectj/aspect-creator-plugin/src/main/java/de/saxsys/mvvmfx/aspectj/aspectcreator/AspectMakerMojo.java
+++ mvvmfx-aspectj/aspect-creator-plugin/src/main/java/de/saxsys/mvvmfx/aspectj/aspectcreator/AspectMakerMojo.java
@@ -6,6 +6,7 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mojo(name = "createaspects")
@@ -15,34 +16,30 @@ public class AspectMakerMojo extends AbstractMojo{
      * Model Packages.
      */
     @Parameter
-    private List<String> modelPackages;
+    private List<String> modelPackages = new ArrayList<>();
 
     /**
      * Model Classes.
      */
     @Parameter
-    private List<String> modelClasses;
+    private List<String> modelClasses = new ArrayList<>();
 
     public void execute() throws MojoExecutionException {
         Log log = getLog();
         log.info("Creating aspects");
 
-        if(modelPackages != null) {
             modelPackages.forEach((String p) -> {
                 p = p.trim() + "..*";
                 Config.addToModel(p);
                 log.info("added as model: "+p);
             });
-        }
 
-        if(modelClasses != null) {
             modelClasses.forEach((String c) -> {
                 c = c.trim();
                 Config.addToModel(c);
                 log.info("added as model: "+c);
 
             });
-        }
 
         CodeGenerator codeGenerator = new CodeGenerator();
         codeGenerator.createAspects();
