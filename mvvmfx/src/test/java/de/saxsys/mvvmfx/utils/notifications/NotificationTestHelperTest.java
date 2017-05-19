0c9ee8c8764db7f0268de192971806fcbabd4bdd
gbalderas <gerardo.balderas@ws072.saxsys.de>
2017-05-19 17:33:55 +0200

    fix #267 add access to received notification list

--- mvvmfx/src/test/java/de/saxsys/mvvmfx/utils/notifications/NotificationTestHelperTest.java
+++ mvvmfx/src/test/java/de/saxsys/mvvmfx/utils/notifications/NotificationTestHelperTest.java
@@ -15,92 +15,94 @@
  ******************************************************************************/
 package de.saxsys.mvvmfx.utils.notifications;
 
-import static org.assertj.core.api.Assertions.*;
-
+import de.saxsys.mvvmfx.ViewModel;
+import javafx.util.Pair;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.saxsys.mvvmfx.ViewModel;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author manuel.mauky
  */
 public class NotificationTestHelperTest {
-	
+
 	public class MyViewModel implements ViewModel {
 	}
-	
+
 	private MyViewModel viewModel;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		viewModel = new MyViewModel();
 	}
-	
+
 	@Test
 	public void singlePublish() {
 		NotificationTestHelper helper = new NotificationTestHelper();
 		viewModel.subscribe("test", helper);
-		
+
 		viewModel.publish("test");
-		
+
 		assertThat(helper.numberOfReceivedNotifications()).isEqualTo(1);
 	}
-	
+
 	@Test
 	public void multiplePublish() {
 		NotificationTestHelper helper = new NotificationTestHelper();
 		viewModel.subscribe("test", helper);
-		
+
 		int n = 10;
-		
+
 		for (int i = 0; i < n; i++) {
 			viewModel.publish("test");
 		}
-		
+
 		assertThat(helper.numberOfReceivedNotifications()).isEqualTo(n);
 	}
-	
+
 	@Test
 	public void globalNotificationCenter() {
 		NotificationTestHelper helper = new NotificationTestHelper();
-		
+
 		NotificationCenter notificationCenter = new DefaultNotificationCenter();
-		
+
 		notificationCenter.subscribe("OK", helper);
-		
-		
+
+
 		notificationCenter.publish("OK");
-		
-		
+
+
 		assertThat(helper.numberOfReceivedNotifications()).isEqualTo(1);
 	}
-	
+
 	@Test
 	public void publishOnOtherThread() {
 		NotificationTestHelper helper = new NotificationTestHelper(150l);
-		
+
 		NotificationCenter notificationCenter = new DefaultNotificationCenter();
-		
+
 		notificationCenter.subscribe("OK", helper);
-		
-		
+
+
 		Runnable r = () -> notificationCenter.publish("OK");
-		
+
 		new Thread(r).start();
-		
+
 		assertThat(helper.numberOfReceivedNotifications()).isEqualTo(1);
 	}
-	
+
 	@Test
 	public void timeout() {
 		NotificationTestHelper helper = new NotificationTestHelper(300l);
-		
+
 		NotificationCenter notificationCenter = new DefaultNotificationCenter();
-		
+
 		notificationCenter.subscribe("OK", helper);
-		
-		
+
+
 		Runnable r = () -> {
 			try {
 				Thread.sleep(100l);
@@ -109,9 +111,50 @@ public class NotificationTestHelperTest {
 			}
 			notificationCenter.publish("OK");
 		};
-		
+
 		new Thread(r).start();
-		
+
 		assertThat(helper.numberOfReceivedNotifications()).isEqualTo(1);
+	}
+
+	@Test
+	public void notificationList() {
+		NotificationTestHelper helper = new NotificationTestHelper();
+		viewModel.subscribe("test", helper);
+		viewModel.subscribe("something", helper);
+
+		viewModel.publish("test", 1, "a", 2, 3, "b");
+
+		Pair<String, Object[]> notification1 = helper.getReceivedNotifications().get(0);
+
+		assertThat(notification1.getValue().length).isEqualTo(5);
+		long integerValueCount = Stream.of(notification1.getValue())
+				.filter(v -> v instanceof Integer)
+				.count();
+
+		long stringValueCount = Stream.of(notification1.getValue())
+				.filter(v -> v instanceof String)
+				.count();
+
+		assertThat(integerValueCount).isEqualTo(3);
+		assertThat(stringValueCount).isEqualTo(2);
+		assertThat(notification1.getValue()[1]).isEqualTo("a");
+
+		//second message
+		viewModel.publish("test", false);
+		Pair<String, Object[]> notification2 = helper.getReceivedNotifications().get(1);
+		assertThat(notification2.getKey()).isEqualTo("test");
+		assertThat(notification2.getValue()[0]).isEqualTo(false);
+
+		//getting an empty message
+		viewModel.publish("something");
+		Pair<String, Object[]> notification3 = helper.getReceivedNotifications().get(2);
+		assertThat(notification3.getKey()).isEqualTo("something");
+		assertThat(notification3.getValue()).isEmpty();
+
+		//message should not be received
+		viewModel.publish("some other message");
+		assertThat(helper.getReceivedNotifications().size()).isEqualTo(3);
+
 	}
 }
