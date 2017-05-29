14a59c0f5e65cf12878c92ecf90b4f0fda95e2a7
Rafael Guillén Sánchez <rafael.guillen@datys.cu>
2017-05-29 10:21:51 -0400

    Annotation to support custom path to fxml file

---
+++ mvvmfx/src/main/java/de/saxsys/mvvmfx/FxmlPath.java
@@ -0,0 +1,38 @@
+package de.saxsys.mvvmfx;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to define a {@link FxmlView} custom
+ * FXML file path.
+ *
+ * Note that the full path to the FXML file must be provided.
+ *
+ * Example: <br>
+ *     <br>
+ * <pre>
+ * package example.view;
+ *
+ *{@literal @}FxmlPath("/fxml/CustomPathView.fxml")
+ * public class CustomView implements {@link FxmlView} {
+ *
+ *         ...
+ *
+ * }
+ * </pre>
+ *
+ * @author rafael.guillen
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface FxmlPath {
+
+    /**
+     * Custom fxml file path, empty by default
+     * @return path to the fxml file
+     */
+    String value() default "";
+}
