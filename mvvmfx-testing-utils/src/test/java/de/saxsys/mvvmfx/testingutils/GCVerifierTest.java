48e9b661d72c331574e375bb23ea0bad14d04761
gbalderas <gerardo.balderas@ws072.saxsys.de>
2017-08-22 17:34:17 +0200

    migrate GCVerifierTest to JUnit5

--- mvvmfx-testing-utils/src/test/java/de/saxsys/mvvmfx/testingutils/GCVerifierTest.java
+++ mvvmfx-testing-utils/src/test/java/de/saxsys/mvvmfx/testingutils/GCVerifierTest.java
@@ -1,6 +1,6 @@
 package de.saxsys.mvvmfx.testingutils;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
