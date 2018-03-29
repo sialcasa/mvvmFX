1f39874a5ecc2726dd17dc00681e1b7d47ab4888
ntherrien <nicolas.therrien@airbus-dscomm.com>
2018-03-29 11:29:50 -0400

    538 Using ControlsFxVisualizer causes node graph inconsistencies

--- mvvmfx-testing-utils/src/main/java/de/saxsys/mvvmfx/testingutils/FxTestingUtils.java
+++ mvvmfx-testing-utils/src/main/java/de/saxsys/mvvmfx/testingutils/FxTestingUtils.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -43,5 +44,36 @@ public class FxTestingUtils {
 	 */
 	public static void waitForUiThread() {
 		waitForUiThread(1000);
+	}
+
+	/**
+	 * This method is used to put the platform thread on hold. In conjunction with waitForUiThread(), it
+	 * allows to test that tasks are being queued on the thread.
+	 *
+	 * For example:
+	 * <pre>
+	 * {@code
+	 * cut.usePlatformRunLaterToDoSomething(...);
+	 * Semaphore semaphore = new Semaphore(0);
+	 * FxTestingUtils.putPlatformThreadOnHold(semaphore);
+	 * Mockito.verifyZeroInteractions(testDummy); // confirms runLater is being used
+	 * semaphore.release();
+	 * FxTestingUtils.waitForUiThread();
+	 * Mockito.verify(testDummy).doSomething();
+	 * }
+	 * </pre>
+	 *
+
+	 * @param semaphore
+	 * @throws InterruptedException
+	 */
+	public static void putPlatformThreadOnHold(Semaphore semaphore) throws InterruptedException {
+		Platform.runLater(() -> {
+			try {
+				semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
