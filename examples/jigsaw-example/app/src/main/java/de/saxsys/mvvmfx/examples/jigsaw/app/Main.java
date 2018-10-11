083ddece3eeebc9d4274fc4ab1464a9ff7ce5e8f
jens.weickert <jens.weickert@saxsys.de>
2018-10-11 13:59:06 +0200

    Disables Java11 for a build under Java8.

--- examples/jigsaw-example/app/src/main/java/de/saxsys/mvvmfx/examples/jigsaw/app/Main.java
+++ examples/jigsaw-example/app/src/main/java/de/saxsys/mvvmfx/examples/jigsaw/app/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage){
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("app");
 
