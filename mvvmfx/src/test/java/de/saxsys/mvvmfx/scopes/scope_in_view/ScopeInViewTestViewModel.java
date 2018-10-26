2c38aae16446733c37b7c5ee85373e6028aa7811
Manuel Mauky <manuel.mauky@gmail.com>
2018-10-26 22:42:26 +0200

    Fix #500. Implement checking of views to prevent injection of scopes

---
+++ mvvmfx/src/test/java/de/saxsys/mvvmfx/scopes/scope_in_view/ScopeInViewTestViewModel.java
@@ -0,0 +1,8 @@
+package de.saxsys.mvvmfx.scopes.scope_in_view;
+
+import de.saxsys.mvvmfx.ScopeProvider;
+import de.saxsys.mvvmfx.ViewModel;
+
+public class ScopeInViewTestViewModel implements ViewModel {
+
+}
