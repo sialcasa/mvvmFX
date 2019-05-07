07b240a9bde0d231412aa7f7e5818b8f9c6c5344
Manuel Mauky <manuel.mauky@gmail.com>
2019-05-07 15:16:52 +0200

    Fix #589. Fixes wrong automatic module names

--- examples/jigsaw-example/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-
-module de.saxsys.mvvmfx.examples.jigsaw{
-
-    requires javafx.base;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires de.saxsys.mvvmfx;
-}
